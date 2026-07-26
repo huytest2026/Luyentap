3a5e93a8bf023680a3e73848511c2181ea6e1c44
huytest2026 <examinationnet.2026@gmail.com>
2026-07-26 23:00:08 +0700

    Update API_URL to new endpoint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyRraHEvuMqEfEEoK68_e0YFaya_sz2dOA5O4BkAyCp3XEtpQSx5WjGubVPu5pt_vh1/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwClcRQ_6XkCq-psx7vOYArfCloZuQ_hBygTWmx_shheM27EaSYlyYUqk-2N97lXqCFew/exec";
 
 let AppState = {
     allQuizData: [],
